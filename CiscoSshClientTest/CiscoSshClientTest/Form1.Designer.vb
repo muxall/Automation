2f50c7f62359fa2315344b5c07dc6b0b2506f80c
Michael Crane <39747564+muxall@users.noreply.github.com>
2018-12-10 17:29:21 -0600

    Add files via upload

---
+++ CiscoSshClientTest/CiscoSshClientTest/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnConnect = New System.Windows.Forms.Button()
+        Me.BtnSend = New System.Windows.Forms.Button()
+        Me.TxtConnectIpAddr = New System.Windows.Forms.TextBox()
+        Me.TxtConnectPort = New System.Windows.Forms.TextBox()
+        Me.TxtSend = New System.Windows.Forms.TextBox()
+        Me.TxtResp = New System.Windows.Forms.TextBox()
+        Me.TimerRecv = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'BtnConnect
+        '
+        Me.BtnConnect.Location = New System.Drawing.Point(336, 33)
+        Me.BtnConnect.Name = "BtnConnect"
+        Me.BtnConnect.Size = New System.Drawing.Size(99, 23)
+        Me.BtnConnect.TabIndex = 0
+        Me.BtnConnect.Text = "Connect"
+        Me.BtnConnect.UseVisualStyleBackColor = True
+        '
+        'BtnSend
+        '
+        Me.BtnSend.Location = New System.Drawing.Point(336, 72)
+        Me.BtnSend.Name = "BtnSend"
+        Me.BtnSend.Size = New System.Drawing.Size(99, 23)
+        Me.BtnSend.TabIndex = 1
+        Me.BtnSend.Text = "Send"
+        Me.BtnSend.UseVisualStyleBackColor = True
+        '
+        'TxtConnectIpAddr
+        '
+        Me.TxtConnectIpAddr.Location = New System.Drawing.Point(89, 33)
+        Me.TxtConnectIpAddr.Name = "TxtConnectIpAddr"
+        Me.TxtConnectIpAddr.Size = New System.Drawing.Size(132, 22)
+        Me.TxtConnectIpAddr.TabIndex = 2
+        Me.TxtConnectIpAddr.Text = "192.168.1.76"
+        '
+        'TxtConnectPort
+        '
+        Me.TxtConnectPort.Location = New System.Drawing.Point(227, 33)
+        Me.TxtConnectPort.Name = "TxtConnectPort"
+        Me.TxtConnectPort.Size = New System.Drawing.Size(87, 22)
+        Me.TxtConnectPort.TabIndex = 3
+        Me.TxtConnectPort.Text = "22"
+        '
+        'TxtSend
+        '
+        Me.TxtSend.Location = New System.Drawing.Point(89, 72)
+        Me.TxtSend.Name = "TxtSend"
+        Me.TxtSend.Size = New System.Drawing.Size(225, 22)
+        Me.TxtSend.TabIndex = 4
+        '
+        'TxtResp
+        '
+        Me.TxtResp.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TxtResp.Location = New System.Drawing.Point(31, 121)
+        Me.TxtResp.Multiline = True
+        Me.TxtResp.Name = "TxtResp"
+        Me.TxtResp.Size = New System.Drawing.Size(380, 224)
+        Me.TxtResp.TabIndex = 5
+        '
+        'TimerRecv
+        '
+        Me.TimerRecv.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(469, 373)
+        Me.Controls.Add(Me.TxtResp)
+        Me.Controls.Add(Me.TxtSend)
+        Me.Controls.Add(Me.TxtConnectPort)
+        Me.Controls.Add(Me.TxtConnectIpAddr)
+        Me.Controls.Add(Me.BtnSend)
+        Me.Controls.Add(Me.BtnConnect)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnConnect As Button
+    Friend WithEvents BtnSend As Button
+    Friend WithEvents TxtConnectIpAddr As TextBox
+    Friend WithEvents TxtConnectPort As TextBox
+    Friend WithEvents TxtSend As TextBox
+    Friend WithEvents TxtResp As TextBox
+    Friend WithEvents TimerRecv As Timer
+End Class
---
+++ CiscoSshClientTest/CiscoSshClientTest/Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System.Text.RegularExpressions
+Imports Renci.SshNet
+Imports Renci.SshNet.Common
+
+Public Class Form1
+    Dim client As SshClient
+    Dim stream As ShellStream
+
+
+    Private Sub BtnConnect_Click() Handles BtnConnect.Click
+        Dim server As String = TxtConnectIpAddr.Text
+        Dim port As Integer = TxtConnectPort.Text
+        Dim username As String = "cisco"
+        Dim password As String = "cisco"
+        Dim connectionInfo = New PasswordConnectionInfo(server, port, username, password)
+        connectionInfo.Timeout = TimeSpan.FromSeconds(5)
+        If BtnConnect.Text Like "Connect" Then
+            Connect(connectionInfo)
+        ElseIf BtnConnect.Text Like "Disconnect" Then
+            Disconnect()
+        End If
+    End Sub
+
+    Private Sub Connect(connectionInfo As [PasswordConnectionInfo])
+        Try
+            ' Create a SshClient
+            client = New SshClient(connectionInfo)
+            client.Connect()
+
+            'If it doesn't throw an exception set the button name to Disconnect.
+            BtnConnect.Text = "Disconnect"
+
+            'Dim dict = New Dictionary(Of TerminalModes, UInt32)
+            'dict.Add(TerminalModes.ECHO, 0)
+            'dict.Item(TerminalModes.ECHO) = 0
+            'stream = client.CreateShellStream("vt100", 80, 24, 800, 600, 1024, dict)
+
+            stream = client.CreateShellStream("CLI", 0, 0, 0, 0, 1024)
+
+            ' disable terminal paging.
+            TxtResp.AppendText("Disable terminal paging." & vbCrLf)
+            stream.WriteLine("terminal length 0")
+            stream.Flush()
+            TimerRecv.Enabled = True
+
+        Catch e As ArgumentException
+            Debug.WriteLine("Host or Username is invalid: {0}", e)
+            BtnConnect.Text = "Connect"
+        Catch e As SshException
+            Debug.WriteLine("SshException: {0}", e)
+            BtnConnect.Text = "Connect"
+        End Try
+    End Sub
+
+    Private Sub BtnSend_Click() Handles BtnSend.Click
+        Try
+            ' Send the message to the connected DUT. 
+            Dim cmd = TxtSend.Text
+            stream.WriteLine(cmd)
+            stream.Flush()
+            TxtResp.AppendText("Sent: " & cmd & vbCrLf)
+            Debug.WriteLine("Sent:" & cmd)
+            TxtSend.Clear()
+            'Enable a 1000ms receive timer to compensate for network delay.
+            TimerRecv.Enabled = True
+        Catch e As ArgumentNullException
+            Debug.WriteLine("ArgumentNullException: {0}", e)
+            'Console.WriteLine("ArgumentNullException: {0}", e)
+        End Try
+    End Sub
+
+    Private Sub Wait() Handles TimerRecv.Tick
+        TimerRecv.Enabled = False
+        GetResponse()
+    End Sub
+
+    Private Sub GetResponse()
+        ' Read the first batch of the SshServer response .
+        If stream.DataAvailable Then
+            Dim strResponse = stream.Read()
+
+            TxtResp.AppendText("Received: " & strResponse & vbCrLf)
+            Debug.WriteLine("Received:" & strResponse)
+        Else
+            TxtResp.AppendText("No response" & vbCrLf)
+            Debug.WriteLine("No response")
+        End If
+
+    End Sub
+
+
+    Private Sub Disconnect()
+        Try
+            TxtResp.AppendText("Disconnecting...")
+            stream.Close()
+            client.Disconnect()
+            BtnConnect.Text = "Connect"
+            TxtResp.AppendText("Done!" & vbCrLf)
+        Catch e As ObjectDisposedException
+            Debug.WriteLine("ObjectDisposedException: {0}", e)
+        End Try
+
+    End Sub
+
+End Class
